b9b5ed1b088d11da44b06dbcf52ae8de254e610f
Gabriel Leclercq <gabrielleclercq@amalgama.co>
2026-02-07 17:11:47 -0300

    Add RSpec, VCR, and WebMock for testing

--- Gemfile
+++ Gemfile
@@ -47,4 +47,9 @@ group :development, :test do
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
+
+  # Testing
+  gem "rspec-rails"
+  gem "vcr"
+  gem "webmock"
 end
---
+++ spec/support/vcr.rb
@@ -0,0 +1,26 @@
+require "vcr"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+
+  config.filter_sensitive_data("<IGDB_CLIENT_ID>") do
+    Rails.application.credentials.dig(:igdb, :client_id)
+  end
+
+  config.filter_sensitive_data("<IGDB_CLIENT_SECRET>") do
+    Rails.application.credentials.dig(:igdb, :client_secret)
+  end
+
+  config.filter_sensitive_data("<IGDB_ACCESS_TOKEN>") do |interaction|
+    if interaction.response.body.include?("access_token")
+      JSON.parse(interaction.response.body)["access_token"] rescue nil
+    end
+  end
+
+  config.default_cassette_options = {
+    record: :new_episodes,
+    match_requests_on: %i[method uri body]
+  }
+end
