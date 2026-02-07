3e0d808752705e0ca7d0558123b7170ce66e793a
Gabriel Leclercq <gabrielleclercq@amalgama.co>
2026-02-07 20:47:23 -0300

    Feat: Add Game and Genre models with migrations

---
+++ db/migrate/20260207222302_create_games.rb
@@ -0,0 +1,25 @@
+class CreateGames < ActiveRecord::Migration[8.1]
+  def change
+    create_table :games do |t|
+      t.bigint :igdb_id
+      t.string :source, null: false, default: "igdb"
+
+      t.string :title, null: false
+      t.string :slug, null: false
+      t.text :description
+      t.string :cover_image_url
+      t.decimal :user_rating, precision: 4, scale: 2
+      t.date :release_date
+
+      t.string :platforms, array: true, default: []
+      t.string :developers, array: true, default: []
+
+      t.timestamps
+    end
+
+    add_index :games, :slug, unique: true
+    add_index :games, :igdb_id
+    add_index :games, [:source, :igdb_id], unique: true
+    add_index :games, :platforms, using: :gin
+  end
+end
